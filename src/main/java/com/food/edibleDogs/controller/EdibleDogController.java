0d7d5bce0014e5d55b3b9e60a0bfa80701416de3
Reisliebhaber <pmntt21@gmail.com>
2022-07-01 21:05:05 +0200

    refactors logic, so that the service method isn't in the controller anymore, instead its in the service class

--- src/main/java/com/food/edibleDogs/controller/EdibleDogController.java
+++ src/main/java/com/food/edibleDogs/controller/EdibleDogController.java
@@ -3,6 +3,8 @@ package com.food.edibleDogs.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.food.edibleDogs.service.EdibleDogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,9 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class EdibleDogController {
 
+    @Autowired
+    EdibleDogService edibleDogService;
+
     @GetMapping("/home")
     public String getHome(Model model) {
         model.addAttribute("message", "Hello fellow Dogs, I AM THE CONTROLLER!!! :D");
@@ -20,23 +25,7 @@ public class EdibleDogController {
 
     @GetMapping("/test")
     public String getTest(Model model) {
-        //add Picture Request here
-        RestTemplate restTemplate = new RestTemplate();
-        String dogApiURL = "https://api.thedogapi.com/";
-        String dogImageURL = "";//"https://cdn2.thedogapi.com/images/jtrRacj_g.jpg";
-        ResponseEntity<String> dogImagesResponse = restTemplate.getForEntity(dogApiURL + "v1/images/search?size=med&" +
-                "mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", String.class);
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            JsonNode root = mapper.readTree(dogImagesResponse.getBody());
-            JsonNode jsonDogImageURL = root.get(0).path("url");
-            dogImageURL = jsonDogImageURL.asText();
-        } catch (JsonProcessingException e) {
-            System.out.println("Error, Dog Image Response from API had an Error");
-            throw new RuntimeException(e);
-        }
-        System.out.println(dogImagesResponse.getBody());
-        //
+        String dogImageURL = edibleDogService.fetchDogImageURL();
         model.addAttribute("dogImage", dogImageURL);
         model.addAttribute("message", "Hello fellow Dogs, I AM THE CONTROLLER!!! :D");
         return "test";
--- src/main/java/com/food/edibleDogs/service/EdibleDogService.java
+++ src/main/java/com/food/edibleDogs/service/EdibleDogService.java
@@ -1,4 +1,40 @@
 package com.food.edibleDogs.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor//(onConstructor = @__(@Autowired))
 public class EdibleDogService {
+
+    public final String dogApiURL = "https://api.thedogapi.com/";
+
+    public String fetchDogImageURL() {
+        String dogImageURL = "";
+        String requestDogImagePath = "v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1";
+        String requestDogImageURL = dogApiURL + requestDogImagePath;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> dogImagesResponse = restTemplate.getForEntity(requestDogImageURL, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(dogImagesResponse.getBody());
+            JsonNode jsonDogImageURL = root.get(0).path("url");
+            dogImageURL = jsonDogImageURL.asText();
+        } catch (JsonProcessingException e) {
+            log.error("Error, Dog Image Response from API had an Error", e);
+            throw new RuntimeException(e);
+        }
+        return dogImageURL;
+    }
 }
